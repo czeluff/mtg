0403e613e2e3bc644fb530be4de6fc58158449fa
Chad Zeluff <czeluff@apple.com>
2022-06-13 09:39:36 -0700

    added multiple page support

--- MTG/topcards.py
+++ MTG/topcards.py
@@ -7,17 +7,24 @@ import sys
 
 
 # Given a format, return a DataFrame containing the card names
-def mtgdecks_staples_frame(mtg_format):
+def mtgdecks_staples_frame(mtg_format, pages=1):
     df = pd.DataFrame()
     df.name = mtg_format
     
-    url = f'https://mtgdecks.net/{mtg_format}/staples'
+    url_base = f'https://mtgdecks.net/{mtg_format}/staples'
     hdr = {'User-Agent': 'Mozilla/5.0'}
-    req = Request(url, headers=hdr)
-    page = urlopen(req)
+    card_list_div = []
 
-    soup = bs4.BeautifulSoup(page, features='html.parser')
-    card_list_div = [div for div in soup.find(id='loadMoreCardsRow') if isinstance(div, bs4.element.Tag)] 
+    for pageNumber in range(0, pages):
+        first_page = (pageNumber == 0)
+
+        url = url_base if first_page else f'{url_base}/page:{pageNumber}'
+        req = Request(url, headers=hdr)
+        page = urlopen(req)
+
+        soup = bs4.BeautifulSoup(page, features='html.parser')
+        section = soup.find(id='loadMoreCardsRow') if first_page else soup
+        [card_list_div.append(div) for div in section if div.name == 'div']
 
     df['Name'] = [card_div.find('b').text for card_div in card_list_div]
     df[f'{mtg_format}'] = [df.size] - df.index
@@ -38,7 +45,9 @@ def run():
     # TODO: Add 'Cube' to formats, parse out of comparison of simpleman and wtwlf123
 
     # Create DataFrames per format
-    dfs = [mtgdecks_staples_frame(mtg_format) for mtg_format in formats]
+    # TODO: Use argparse to clean up argument passing
+    pages = int(sys.argv[2]) if len(sys.argv) == 3 else 1
+    dfs = [mtgdecks_staples_frame(mtg_format, pages=pages) for mtg_format in formats]
 
     # Merge all DataFrames into one
     df_merged = reduce(lambda left, right: pd.merge(left, right, on=['Name'], how='outer'), dfs)
@@ -49,7 +58,7 @@ def run():
     df_merged['Total'] = df_total
 
     # Add pricing information
-    if len(sys.argv) == 2:
+    if len(sys.argv) >= 2:
         filename = sys.argv[1]
         f = open(filename)
         cards = json.load(f)
@@ -72,4 +81,5 @@ def run():
         # df_merged['Value'] = [1] - ((value / [max_value]) / df_merged['Count'])
 
     # Export to CSV
-    df_merged.sort_values(by=['Count', 'Value'], ascending=[False, True]).to_csv('mtg.csv')
+    # df_merged.sort_values(by=['Count', 'Value'], ascending=[False, True]).to_csv('mtg.csv')
+    df_merged.sort_values(by=['Count'], ascending=[False]).to_csv('mtg.csv')
